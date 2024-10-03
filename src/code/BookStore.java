6ec36b2fddfc2aaf27245f0d29f7e5a9d5b360f6
Bryson Lindy <bslindy@gmail.com>
2024-10-03 10:20:41 -0700

    added classes

---
+++ src/code/BookStore.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class BookStore
+{
+    private final String storeName;
+    private final List<Book> books;
+
+    public BookStore(final String storeName)
+    {
+        validateName(storeName);
+        this.storeName = storeName;
+        this.books = Book.createBookList();
+    }
+
+    private static void validateName(final String storeName)
+    {
+        if(storeName == null || storeName.isEmpty())
+        {
+            throw new IllegalArgumentException("Store name cannot be null or empty");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
