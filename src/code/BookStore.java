7c6bc891006effc65539bc45d08dcff14f55fb63
Bryson Lindy <bslindy@gmail.com>
2024-10-04 11:31:57 -0700

    added method stubs for required methods

--- src/code/BookStore.java
+++ src/code/BookStore.java
@@ -20,10 +20,69 @@ public class BookStore
         }
     }
 
-    public static void main(String[] args)
+    // TODO prints all titles in UPPERCASE
+    public void printAllTitles()
     {
 
     }
 
+    // TODO prints all Book titles that contain the pass parameter
+    public void printBookTitle(final String title)
+    {
 
+    }
+
+    // TODO prints all titles in alphabetical order
+    public void printTitlesInAlphaOrder()
+    {
+
+    }
+
+    // TODO prints all books of a given decade
+    // ie 2000 would print 2000-2009
+    public void printGroupByDecade(final int decade)
+    {
+
+    }
+
+    // TODO return the longest string in the bookstore
+    public String getLongest()
+    {
+        return "";
+    }
+
+    // TODO return true if a book was written in the given year, false if not
+    public boolean isThereABookWrittenIn(final int year)
+    {
+        return false;
+    }
+
+    // TODO return an int of how many books contain the work passed as parameter
+    public int howManyBooksContain(final String word)
+    {
+        return 0;
+    }
+
+    // TODO return percentage of total books written between ranges as a double
+    public double whichPercentWrittenBetween(final int first, final int last)
+    {
+        return 0;
+    }
+
+    // TODO return the oldest book in the bookstore
+    public Book getOldestBook()
+    {
+        return null;
+    }
+
+    // TODO add all books of given length to a List and return the List
+    public List<Book> getBooksThisLength(final int length)
+    {
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
